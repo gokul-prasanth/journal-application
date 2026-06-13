bae4078bf4f475299b2deb29098b300e8d0c7c6d
Gokul <prasanthgokulgp@gmail.com>
2026-06-13 14:25:43 -0400

    Service classes

---
+++ src/main/java/com/gokul/journal/application/service/JournalEntryService.java
@@ -0,0 +1,70 @@
+package com.gokul.journal.application.service;
+
+import com.gokul.journal.application.entity.JournalEntry;
+import com.gokul.journal.application.entity.User;
+import com.gokul.journal.application.repos.JournalEntryRepository;
+
+import org.bson.types.ObjectId;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Optional;
+
+@Component
+public class JournalEntryService {
+
+    @Autowired
+    private JournalEntryRepository journalEntryRepository;
+
+    @Autowired
+    private UserService userService;
+
+    @Transactional
+    public void saveEntry(JournalEntry journalEntry, String userName) {
+        try {
+            //find user based on userName
+            User user = userService.findByUserName(userName);
+            journalEntry.setDate(LocalDateTime.now());
+            JournalEntry saved = journalEntryRepository.save(journalEntry);
+            user.getJournalEntries().add(saved);
+            //saving from in-memory to user repo.
+            userService.saveUser(user);
+        } catch (Exception e) {
+            throw new RuntimeException("An Error occurred while saving entry " + e);
+        }
+
+    }
+
+    public void saveEntry(JournalEntry journalEntry) {
+        journalEntryRepository.save(journalEntry);
+    }
+
+    public List<JournalEntry> getAll() {
+        return journalEntryRepository.findAll();
+    }
+
+    public Optional<JournalEntry> findById(ObjectId id) {
+        return journalEntryRepository.findById(id);
+    }
+
+    @Transactional
+    public boolean deleteById(ObjectId id, String userName) {
+        boolean removed = false;
+        try {
+            User user = userService.findByUserName(userName);
+            removed = user.getJournalEntries().removeIf(x -> x.getId().equals(id));
+            if (removed) {
+                userService.saveUser(user);
+                journalEntryRepository.deleteById(id);
+            }
+        } catch (Exception e) {
+            System.out.println(e);
+            throw new RuntimeException("An error occurred while deleting entry ", e);
+        }
+        return removed;
+    }
+    
+}
